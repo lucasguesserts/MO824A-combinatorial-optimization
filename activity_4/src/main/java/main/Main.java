82b38bf8a3f3180b90e3c21d4308d41c07c4a0f9
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-05-11 18:27:06 -0300

    feat(activity_4): use command line arguments in 'main'

--- activity_4/src/main/java/main/Main.java
+++ activity_4/src/main/java/main/Main.java
@@ -8,8 +8,19 @@ import problems.kqbf.solvers.GRASP_KQBF;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        KQBF.solve("instances/qbf/qbf040");
-        GRASP_KQBF.staticSolve("instances/kqbf/kqbf100");
+        if (args.length != 2) {
+            System.out.println("usage: gradle run --args=\"ALGORITHM INSTANCE\"");
+            System.out.println("ALGORITHM: qbf, kqbf");
+            System.out.println("INSTANCE: instances/qbf/*, instances/kqbf/*");
+            return;
+        }
+        if (args[0].equals("qbf")) {
+            KQBF.solve(args[1]);
+        }
+        else if (args[0].equals("kqbf")) {
+            GRASP_KQBF.staticSolve(args[1]);
+        }
+        return;
     }
 
 }
