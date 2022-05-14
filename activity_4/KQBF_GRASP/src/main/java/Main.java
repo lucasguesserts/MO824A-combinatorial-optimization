eb03037bbb90a375b2a9b9b01bc89388a6cf60bc
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-05-13 23:27:38 -0300

    feat: change KQBF_GRASP to run algorithm with each instances and each construction mechanism

--- activity_4/KQBF_GRASP/src/main/java/Main.java
+++ activity_4/KQBF_GRASP/src/main/java/Main.java
@@ -1,27 +1,54 @@
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
-import grasp.GRASP_KQBF;
 import grasp.AbstractGRASP.ConstructionMechanism;
+import grasp.GRASP_KQBF;
 import solutions.Solution;
 
 public class Main {
 
     private static String INSTANCES_DIR = "../instances/kqbf/";
+    private static List<String> instaceList = Arrays.asList(
+        "kqbf020",
+        "kqbf040",
+        "kqbf060",
+        "kqbf080",
+        "kqbf100",
+        "kqbf200",
+        "kqbf400"
+    );
 
     private static Double alpha = 0.5;
     private static Integer iterations = 1000;
     private static Boolean firstImproving = false;
-    private static ConstructionMechanism constructionMechanism = ConstructionMechanism.DEFAULT;
 
     public static void main(String[] args) throws IOException {
-        final String instance = INSTANCES_DIR + "kqbf020";
+        for (final var instance: instaceList) {
+            final var fileName = INSTANCES_DIR + instance;
+            for (final var constructionMechanism: ConstructionMechanism.values()) {
+                run_algorithm(constructionMechanism, fileName);
+            }
+        }
+    }
+
+    private static void run_algorithm(
+        final ConstructionMechanism constructionMechanism,
+        final String fileName
+    ) throws IOException {
         final long startTime = System.currentTimeMillis();
-        final GRASP_KQBF grasp = new GRASP_KQBF(alpha, iterations, firstImproving, constructionMechanism, instance);
+        System.out.println("\n\n=============================");
+        System.out.println("instance: " + fileName);
+        System.out.println("construction mechanist: " + fileName);
+        System.out.println("-----------------");
+        final GRASP_KQBF grasp = new GRASP_KQBF(alpha, iterations, firstImproving, constructionMechanism, fileName);
         final Solution<Integer> bestSolution = grasp.solve();
         final Integer knapsackWeight = grasp.getKnapsackWeightOfSolution();
         final long endTime = System.currentTimeMillis();
         final long totalTime = endTime - startTime;
-        System.out.println("");
+        System.out.println("-----------------");
+        System.out.println("instance: " + fileName);
+        System.out.println("construction mechanist: " + fileName);
         System.out.println("Best Solution Found: " + bestSolution);
         System.out.println("Knapsack Weight of Best Solution: " + knapsackWeight);
         System.out.println("RunningTime: " + (double) totalTime / (double) 1000 + " seconds");
