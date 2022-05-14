4fd8c4c8df6a3ebefd61070f7c264caa4068e170
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-05-13 23:30:35 -0300

    feat: change KQBF_GRASP to run algorithm with some different values of alpha

--- activity_4/KQBF_GRASP/src/main/java/Main.java
+++ activity_4/KQBF_GRASP/src/main/java/Main.java
@@ -18,28 +18,35 @@ public class Main {
         "kqbf200",
         "kqbf400"
     );
+    private static List<Double> ALPHA_LIST = Arrays.asList(
+        0.2,
+        0.5
+    );
 
-    private static Double alpha = 0.5;
     private static Integer iterations = 1000;
     private static Boolean firstImproving = false;
 
     public static void main(String[] args) throws IOException {
-        for (final var instance: instaceList) {
-            final var fileName = INSTANCES_DIR + instance;
-            for (final var constructionMechanism: ConstructionMechanism.values()) {
-                run_algorithm(constructionMechanism, fileName);
+        for (final var alpha: ALPHA_LIST) {
+            for (final var instance: instaceList) {
+                final var fileName = INSTANCES_DIR + instance;
+                for (final var constructionMechanism: ConstructionMechanism.values()) {
+                    run_algorithm(alpha, constructionMechanism, fileName);
+                }
             }
         }
     }
 
     private static void run_algorithm(
+        final Double alpha,
         final ConstructionMechanism constructionMechanism,
         final String fileName
     ) throws IOException {
         final long startTime = System.currentTimeMillis();
         System.out.println("\n\n=============================");
+        System.out.println("alpha: " + alpha);
+        System.out.println("construction mechanist: " + constructionMechanism);
         System.out.println("instance: " + fileName);
-        System.out.println("construction mechanist: " + fileName);
         System.out.println("-----------------");
         final GRASP_KQBF grasp = new GRASP_KQBF(alpha, iterations, firstImproving, constructionMechanism, fileName);
         final Solution<Integer> bestSolution = grasp.solve();
@@ -48,7 +55,8 @@ public class Main {
         final long totalTime = endTime - startTime;
         System.out.println("-----------------");
         System.out.println("instance: " + fileName);
-        System.out.println("construction mechanist: " + fileName);
+        System.out.println("construction mechanist: " + constructionMechanism);
+        System.out.println("alpha: " + alpha);
         System.out.println("Best Solution Found: " + bestSolution);
         System.out.println("Knapsack Weight of Best Solution: " + knapsackWeight);
         System.out.println("RunningTime: " + (double) totalTime / (double) 1000 + " seconds");
