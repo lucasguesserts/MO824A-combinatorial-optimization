c05cf89af85bd4507c1f02c110a64df934005535
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-05-19 06:52:40 -0300

    feat(activity_5): fork qbf project with name qbf_random
    One is going to change qbf project to use tabu search

---
+++ activity_5/qbf_random/src/main/java/Main.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+
+import SolutionCost.SolutionCostInteger;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+
+        {
+            final SolutionCostInteger solution = new SolutionCostInteger("../instances/qbf/qbf040");
+            SolutionCostInteger bestSolution = new SolutionCostInteger(solution);
+            for (Integer i = 0; i < 100000; i++) {
+                for (Integer j = 0; j < solution.getDomainSize(); j++) {
+                    if (Math.random() < 0.5) {
+                        solution.add(j);
+                    }
+                }
+                // System.out.println(String.format("iteration %d", i));
+                if (bestSolution.getCost() < solution.getCost())
+                    bestSolution = new SolutionCostInteger(solution);
+                solution.reset();
+            }
+            System.out.println(String.format(
+                "Best solution found: \n\t%s",
+                bestSolution.toString()
+            ));
+        }
+
+        { // evaluates the all-ones array.
+            final SolutionCostInteger solution = new SolutionCostInteger("../instances/qbf/qbf040");
+            for (Integer j = 0; j < solution.getDomainSize(); j++) {
+                solution.add(j);
+            }
+            System.out.println(String.format(
+                "All ones solution: \n\t%s",
+                solution.toString()
+            ));
+        }
+    }
+}
