a4c11ec52b1665ad72919d3ddba68a7150ccd293
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-05-20 12:02:53 -0300

    refactor(activity_5): generalize 'InputReaderQBF' in order to support extensions

--- activity_5/tabu_search/src/main/java/inputReader/InputReaderQBF.java
+++ activity_5/tabu_search/src/main/java/inputReader/InputReaderQBF.java
@@ -9,24 +9,14 @@ import java.util.Arrays;
 
 public class InputReaderQBF {
 
-    private final Integer size;
-    private final Integer[][] matrix;
+    protected StreamTokenizer stok;
+    protected Integer size;
+    protected Integer[][] matrix;
 
     public InputReaderQBF(final String fileName) throws IOException {
         final Reader file = new BufferedReader(new FileReader(fileName));
-        final StreamTokenizer stok = new StreamTokenizer(file);
-        stok.nextToken();
-        this.size = (int) stok.nval;
-        this.matrix = new Integer[this.size][this.size];
-        for (int i = 0; i < this.size; ++i) {
-            Arrays.fill(this.matrix[i], 0);
-        }
-        for (int i = 0; i < this.size; ++i) {
-            for (int j = i; j < this.size; ++j) {
-                stok.nextToken();
-                this.matrix[i][j] = (int) Math.round(stok.nval);
-            }
-        }
+        this.stok = new StreamTokenizer(file);
+        this.construct();
     }
 
     public Integer getSize() {
@@ -35,6 +25,49 @@ public class InputReaderQBF {
 
     public Integer[][] getMatrix() {
         return this.matrix;
+    }
+
+    public void printMatrix() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(String.format("%3d ", this.matrix[i][j]));
+            }
+            System.out.println();
+        }
+    }
+
+    // Beware that the order in which you use the methods below matter a lot!
+
+    protected void construct() throws IOException {
+        // override this method in child classes
+        this.size = this.readInteger();
+        this.matrix = this.allocateMatrix();
+        this.fillMatrixWithZeros();
+        this.readMatrix();
+    }
+
+    protected Integer readInteger() throws IOException {
+        this.stok.nextToken();
+        return (int) this.stok.nval;
+    }
+
+    protected Integer[][] allocateMatrix() {
+        return new Integer[this.size][this.size];
+    }
+
+    protected void fillMatrixWithZeros() {
+        for (int i = 0; i < this.size; ++i) {
+            Arrays.fill(this.matrix[i], 0);
+        }
+    }
+
+    protected void readMatrix() throws IOException {
+        for (int i = 0; i < this.size; ++i) {
+            for (int j = i; j < this.size; ++j) {
+                this.stok.nextToken();
+                this.matrix[i][j] = (int) Math.round(stok.nval);
+            }
+        }
     }
 
 }
