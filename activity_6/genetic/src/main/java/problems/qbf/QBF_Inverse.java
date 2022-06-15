f768cb0727390aa264b768f1d97a362c7bd0c37b
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-06-15 08:36:01 -0300

    refactor(activity_6): remove comments and extra spaces from code

--- activity_6/genetic/src/main/java/problems/qbf/QBF_Inverse.java
+++ activity_6/genetic/src/main/java/problems/qbf/QBF_Inverse.java
@@ -2,59 +2,31 @@ package problems.qbf;
 
 import java.io.IOException;
 
-/**
- * Class representing the inverse of the Quadractic Binary Function
- * ({@link QBF}), which is used since the GRASP is set by
- * default as a minimization procedure.
- * 
- * @author ccavellucci, fusberti
- */
+
 public class QBF_Inverse extends QBF {
 
-	/**
-	 * Constructor for the QBF_Inverse class.
-	 * 
-	 * @param filename
-	 *            Name of the file for which the objective function parameters
-	 *            should be read.
-	 * @throws IOException
-	 *             Necessary for I/O operations.
-	 */
-	public QBF_Inverse(String filename) throws IOException {
-		super(filename);
-	}
+    public QBF_Inverse(String filename) throws IOException {
+        super(filename);
+    }
 
+    @Override
+    public Double evaluateQBF() {
+        return -super.evaluateQBF();
+    }
 
-	/* (non-Javadoc)
-	 * @see problems.qbf.QBF#evaluate()
-	 */
-	@Override
-	public Double evaluateQBF() {
-		return -super.evaluateQBF();
-	}
-	
-	/* (non-Javadoc)
-	 * @see problems.qbf.QBF#evaluateInsertion(int)
-	 */
-	@Override
-	public Double evaluateInsertionQBF(int i) {	
-		return -super.evaluateInsertionQBF(i);
-	}
-	
-	/* (non-Javadoc)
-	 * @see problems.qbf.QBF#evaluateRemoval(int)
-	 */
-	@Override
-	public Double evaluateRemovalQBF(int i) {
-		return -super.evaluateRemovalQBF(i);
-	}
-	
-	/* (non-Javadoc)
-	 * @see problems.qbf.QBF#evaluateExchange(int, int)
-	 */
-	@Override
-	public Double evaluateExchangeQBF(int in, int out) {
-		return -super.evaluateExchangeQBF(in,out);
-	}
+    @Override
+    public Double evaluateInsertionQBF(int i) {
+        return -super.evaluateInsertionQBF(i);
+    }
+
+    @Override
+    public Double evaluateRemovalQBF(int i) {
+        return -super.evaluateRemovalQBF(i);
+    }
+
+    @Override
+    public Double evaluateExchangeQBF(int in, int out) {
+        return -super.evaluateExchangeQBF(in,out);
+    }
 
 }
--- activity_6/genetic/src/main/java/solutions/Solution.java
+++ activity_6/genetic/src/main/java/solutions/Solution.java
@@ -2,24 +2,22 @@ package solutions;
 
 import java.util.ArrayList;
 
-@SuppressWarnings("serial")
 public class Solution<E> extends ArrayList<E> {
-	
-	public Double cost = Double.POSITIVE_INFINITY;
-	
-	public Solution() {
-		super();
-	}
-	
-	public Solution(Solution<E> sol) {
-		super(sol);
-		cost = sol.cost;
-	}
 
-	@Override
-	public String toString() {
-		return "Solution: cost=[" + cost + "], size=[" + this.size() + "], elements=" + super.toString();
-	}
+    public Double cost = Double.POSITIVE_INFINITY;
+
+    public Solution() {
+        super();
+    }
+
+    public Solution(Solution<E> sol) {
+        super(sol);
+        cost = sol.cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Solution: cost=[" + cost + "], size=[" + this.size() + "], elements=" + super.toString();
+    }
 
 }
-
