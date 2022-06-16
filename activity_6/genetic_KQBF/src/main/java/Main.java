291624afddf9bf4a4c9e35a84d09cc9a6e55ac7b
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-06-16 08:56:01 -0300

    feat(activity_6): configure main to run all instances

--- activity_6/genetic_KQBF/src/main/java/Main.java
+++ activity_6/genetic_KQBF/src/main/java/Main.java
@@ -8,7 +8,13 @@ public class Main {
     private static final int populationSize = 100;
     private static final double mutationRate = 1.0 / 100.0;
     private static final List<String> problemInstanceList = Arrays.asList(
-        "../instances/kqbf/kqbf020"
+        "../instances/kqbf/kqbf020",
+        "../instances/kqbf/kqbf040",
+        "../instances/kqbf/kqbf060",
+        "../instances/kqbf/kqbf080",
+        "../instances/kqbf/kqbf100",
+        "../instances/kqbf/kqbf200",
+        "../instances/kqbf/kqbf400"
     );
 
     public static void main(final String[] args) throws IOException {
