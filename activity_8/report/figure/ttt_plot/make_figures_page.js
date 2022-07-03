4325c1951359917d955c491831cb1fbb0b50e455
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-07-03 13:53:27 -0300

    docs(activity_8): add TTT plots to report

---
+++ activity_8/report/figure/ttt_plot/make_figures_page.js
@@ -0,0 +1,64 @@
+const fs = require('fs')
+
+let files = fs.readdirSync('./')
+files = files.filter(f => f.includes('.jpeg'))
+const cases = Array.from(new Set(files.map(
+    fileName => fileName
+        .replace('-exp','')
+        .replace('-qq','')
+        .replace('.jpeg','')
+)))
+
+// const names = Array.from(new Set(cases.map(
+//     c => c.replace(/kqbf([0-9])+-/,'')
+// )))
+// console.log(names)
+
+const nameMap = {
+    'genetic-steady-100-1': 'GA steady',
+    'genetic-vanilla-100-05': 'GA vanilla',
+    'grasp-best': 'GRASP Best',
+    'grasp-first': 'GRASP First',
+    'tabu-best-vanilla-4': 'Tabu vanilla',
+}
+
+function getName (c) {
+    const pureCaseName = c.replace(/kqbf([0-9])+-/,'')
+    const problemInstanceName = c.match(/kqbf([0-9])+/)[0]
+    return {
+        algorithm: nameMap[pureCaseName],
+        problemInstanceName: problemInstanceName
+    }
+
+}
+
+const latexFigures = cases.map(c => {
+    const names = getName(c)
+    return `
+\\begin{figure}[H]
+    \\centering
+    \\begin{subfigure}{0.49\\textwidth}
+        \\includegraphics[width=\\textwidth]{figure/ttt_plot/${c}-exp.jpeg}
+        \\caption{Algorithm ${names.algorithm} - Problem ${names.problemInstanceName}}
+        \\label{fig:${names.algorithm.toLowerCase().replace(' ','-')}-${names.problemInstanceName}-exp}
+    \\end{subfigure}
+    \\hfill
+    \\begin{subfigure}{0.49\\textwidth}
+        \\includegraphics[width=\\textwidth]{figure/ttt_plot/${c}-qq.jpeg}
+        \\caption{Second subfigure.}
+        \\label{fig:${names.algorithm.toLowerCase().replace(' ','-')}-${names.problemInstanceName}-qq}
+    \\end{subfigure}
+    \\caption{Creating subfigures in \\LaTeX.}
+    \\label{fig:${names.algorithm.toLowerCase().replace(' ','-')}-${names.problemInstanceName}}
+\\end{figure}
+`
+})
+
+const allFiguresTogether =
+    '\\section{\\tttfull (\\ttt)}\n' +
+    latexFigures.join('\n')
+
+fs.writeFileSync(
+    '../../appendix/ttt_plot.tex',
+    allFiguresTogether
+)
