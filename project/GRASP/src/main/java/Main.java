47e9a7668a9c72a35f1446cb561bb85c0d50ee26
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-07-12 10:32:21 -0300

    feat(project): initialize GRASP project

---
+++ project/GRASP/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(final String[] args) {
+        System.out.println(String.format(
+            "Running with args: %s",
+            args.toString()
+        ));
+    }
+}
