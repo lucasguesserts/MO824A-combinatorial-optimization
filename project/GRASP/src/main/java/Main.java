5df700b5d2841105eb1e6ee93bae4d6b4f8ae7f0
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-07-12 10:40:25 -0300

    feat(project): add guava graphs

--- project/GRASP/src/main/java/Main.java
+++ project/GRASP/src/main/java/Main.java
@@ -1,8 +1,22 @@
+import java.util.Set;
+
+import com.google.common.graph.GraphBuilder;
+import com.google.common.graph.ImmutableGraph;
+
 public class Main {
     public static void main(final String[] args) {
+        ImmutableGraph<Integer> graph =
+            GraphBuilder.directed()
+                .<Integer>immutable()
+                .addNode(1)
+                .putEdge(2, 3) // also adds nodes 2 and 3 if not already present
+                .putEdge(2, 3) // no effect; Graph does not support parallel edges
+                .build();
+
+        Set<Integer> successorsOfTwo = graph.successors(2); // returns {3}
         System.out.println(String.format(
-            "Running with args: %s",
-            args.toString()
+            "successor of 2: %s",
+            successorsOfTwo.toString()
         ));
     }
 }
