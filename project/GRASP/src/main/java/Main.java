70209b87b355e52055f15f4f296d5a7b34ca33eb
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-07-12 12:20:01 -0300

    feat(project): add class MutableGraph

--- project/GRASP/src/main/java/Main.java
+++ project/GRASP/src/main/java/Main.java
@@ -1,22 +1,50 @@
-import java.util.Set;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import com.google.common.graph.GraphBuilder;
-import com.google.common.graph.ImmutableGraph;
+import com.google.common.graph.MutableGraph;
 
 public class Main {
-    public static void main(final String[] args) {
-        ImmutableGraph<Integer> graph =
-            GraphBuilder.directed()
-                .<Integer>immutable()
-                .addNode(1)
-                .putEdge(2, 3) // also adds nodes 2 and 3 if not already present
-                .putEdge(2, 3) // no effect; Graph does not support parallel edges
-                .build();
+    public static void main(final String[] args) throws IOException {
+        // System.out.println(System.getProperty("user.dir"));
+        final var obj = Auxiliary.readJson("../instances/instance_1.json");
+        final MutableGraph<Integer> graph = GraphBuilder.directed().build();
+        Auxiliary.addNodes(graph, obj.getInt("number_of_nodes"));
+        Auxiliary.addEdges(graph, obj.getJSONArray("edges"));
+        for (final var n : graph.nodes()) {
+            System.out.println(String.format("mutable node: %d", n));
+        }
+        for (final var e : graph.edges()) {
+            System.out.println(String.format("mutable edge: %d %d", e.nodeU(), e.nodeV()));
+        }
+    }
+}
 
-        Set<Integer> successorsOfTwo = graph.successors(2); // returns {3}
-        System.out.println(String.format(
-            "successor of 2: %s",
-            successorsOfTwo.toString()
-        ));
+class Auxiliary {
+    static JSONObject readJson(final String fileName) throws IOException {
+        final var filePath = Path.of(fileName);
+        final var fileContent = Files.readString(filePath);
+        final var obj = new JSONObject(fileContent);
+        return obj;
+    }
+
+    static void addNodes(final MutableGraph<Integer> graph, final Integer numberOfNodes) {
+        for(int i = 0; i < numberOfNodes; ++i) {
+            graph.addNode(i);
+        }
+    }
+
+    static void addEdges(final MutableGraph<Integer> graph, final JSONArray edges) {
+        for(int e = 0; e < edges.length(); ++e) {
+            final JSONArray edgeArray = edges.getJSONArray(e);
+            graph.putEdge(
+                edgeArray.getInt(0),
+                edgeArray.getInt(1)
+            );
+        }
     }
 }
