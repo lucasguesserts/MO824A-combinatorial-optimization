d8997fa39367aa7baaa45056b1910bac9a752a39
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-07-14 14:13:57 -0300

    feat(project): add 'findLeafNodes' to 'GraphTools'

--- project/GRASP/src/main/java/graph/GraphTools.java
+++ project/GRASP/src/main/java/graph/GraphTools.java
@@ -19,4 +19,16 @@ public interface GraphTools {
         return rootNodes;
     }
 
+    public static Set<Integer> findLeafNodes(final Graph<Integer> graph) {
+        final Set<Integer> leafNodes = new HashSet<>(graph
+            .nodes()
+            .stream()
+            .map(node -> new ElementValuePair<Integer>(node, graph.outDegree(node)))
+            .filter(pair -> pair.value.equals(0)) // no successor
+            .map(pair -> pair.element) // get node
+            .toList()
+        );
+        return leafNodes;
+    }
+
 }
