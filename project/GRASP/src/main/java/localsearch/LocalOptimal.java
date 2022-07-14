2f61c07e6003de95f9be63fe604c9fced9094317
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-07-14 15:19:56 -0300

    feat(project): implement 'substituteElement' in 'LocalOptimal'

--- project/GRASP/src/main/java/localsearch/LocalOptimal.java
+++ project/GRASP/src/main/java/localsearch/LocalOptimal.java
@@ -1,8 +1,13 @@
 package localsearch;
 
 import java.util.HashSet;
+import java.util.Set;
+
+import com.google.common.graph.Graphs;
 
 import graph.ElementValuePair;
+import graph.GraphTools;
+import graph.NodeSubstitutionTriple;
 import metaheuristic.greedy_criteria.GreedyCriteria;
 import problem.Problem;
 import solution.Solution;
@@ -14,7 +19,7 @@ public class LocalOptimal implements LocalSearch {
 
     private Solution currentSolution;
     private Boolean elementHasBeenAdded;
-    private Boolean elementHasBeenReplaced;
+    private Boolean elementHasBeenSubstituted;
 
     public LocalOptimal(
         final Problem problem,
@@ -30,7 +35,7 @@ public class LocalOptimal implements LocalSearch {
             this.setNothingDone();
             this.addElement();
             if (this.elementHasBeenAdded) continue;
-            this.replaceElement();
+            this.substituteElement();
         }
         return this.currentSolution;
     }
@@ -38,12 +43,12 @@ public class LocalOptimal implements LocalSearch {
     private void setInitialState(final Solution initialSoution) {
         this.currentSolution = initialSoution;
         this.elementHasBeenAdded = Boolean.TRUE;
-        this.elementHasBeenReplaced = Boolean.TRUE;
+        this.elementHasBeenSubstituted = Boolean.TRUE;
     }
 
     private void setNothingDone() {
         this.elementHasBeenAdded = Boolean.FALSE;
-        this.elementHasBeenReplaced = Boolean.FALSE;
+        this.elementHasBeenSubstituted = Boolean.FALSE;
     }
 
     private void addElement() {
@@ -82,10 +87,59 @@ public class LocalOptimal implements LocalSearch {
         return;
     }
 
-    private void replaceElement() {}
+    private void substituteElement() {
+        final var elementsOfCurrentSolution = this.currentSolution.getElements();
+        final var allSuccessors = new HashSet<>(elementsOfCurrentSolution
+            .stream()
+            .flatMap(node -> this.problem.getGraph().successors(node).stream())
+            .toList()
+        );
+        final var allPossibleCandidatesToAdd = new HashSet<>(allSuccessors
+            .stream()
+            .filter(node -> !elementsOfCurrentSolution.contains(node))
+            .toList()
+        );
+        final var subGraphInducedBySolution = Graphs.inducedSubgraph(this.problem.getGraph(), elementsOfCurrentSolution);
+        final var leafNodes = GraphTools.findLeafNodes(subGraphInducedBySolution);
+        final var weightOfCurrentSolution = this.currentSolution.getWeight();
+        final var elementWeightMap = this.problem.getWeightMap();
+        final var bestSubstitution = leafNodes
+            .stream()
+            .flatMap(node -> {
+                final var successorsOfLeaf = this.problem.getGraph().successors(node);
+                final Set<Integer> candidateList = new HashSet<>(allPossibleCandidatesToAdd);
+                candidateList.removeAll(successorsOfLeaf);
+                final var candidatePairs = candidateList
+                    .stream()
+                    .map(candidate -> new ElementValuePair<>(node, candidate));
+                return candidatePairs;
+            })
+            .map(pair -> {
+                final var elementToRemove = pair.element;
+                final var elementToAdd = pair.value;
+                final var weightOfSubstitution = weightOfCurrentSolution
+                    .subtract(elementWeightMap.get(elementToRemove))
+                    .add(elementWeightMap.get(elementToAdd));
+                return new NodeSubstitutionTriple(elementToRemove, elementToAdd, weightOfSubstitution);
+            })
+            .filter(triple -> triple.weightOfSubstitution.getNorm2() < weightOfCurrentSolution.getNorm2()) // TODO: replace it by a greedy criteria
+            .min((lhs, rhs) -> (int) Math.signum(lhs.weightOfSubstitution.getNorm2() - rhs.weightOfSubstitution.getNorm2()));
+        if (bestSubstitution.isPresent()) {
+            this.elementHasBeenSubstituted = Boolean.TRUE;
+            final var elementToRemove = bestSubstitution.get().toRemove;
+            final var weightOfElementToRemove = elementWeightMap.get(elementToRemove);
+            final var elementToAdd = bestSubstitution.get().toAdd;
+            final var weightOfElementToAdd = elementWeightMap.get(elementToAdd);
+            this.currentSolution.substituteElement(elementToRemove, weightOfElementToRemove, elementToAdd, weightOfElementToAdd);
+        }
+        else {
+            this.elementHasBeenAdded = Boolean.FALSE;
+        }
+        return;
+    }
 
     private Boolean stopCriteriaMet() {
         // stop when nothing has been done in the last iteration
-        return !this.elementHasBeenAdded && !this.elementHasBeenReplaced;
+        return !this.elementHasBeenAdded && !this.elementHasBeenSubstituted;
     }
 }
