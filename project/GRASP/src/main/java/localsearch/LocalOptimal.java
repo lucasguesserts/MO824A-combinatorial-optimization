6650e011f62f6544150acbcb0488c958c9ddf3a3
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-07-14 14:12:14 -0300

    feat(project): implement method 'addElement' of 'LocalOptimal'

--- project/GRASP/src/main/java/localsearch/LocalOptimal.java
+++ project/GRASP/src/main/java/localsearch/LocalOptimal.java
@@ -1,14 +1,28 @@
 package localsearch;
 
+import java.util.HashSet;
+
+import graph.ElementValuePair;
+import metaheuristic.greedy_criteria.GreedyCriteria;
+import problem.Problem;
 import solution.Solution;
 
 public class LocalOptimal implements LocalSearch {
+
+    private final Problem problem;
+    private final GreedyCriteria greedyCriteria;
 
     private Solution currentSolution;
     private Boolean elementHasBeenAdded;
     private Boolean elementHasBeenReplaced;
 
-    public LocalOptimal() {}
+    public LocalOptimal(
+        final Problem problem,
+        final GreedyCriteria greedyCriteria
+    ) {
+        this.problem = problem;
+        this.greedyCriteria = greedyCriteria;
+    }
 
     public Solution search(final Solution initialSoution) {
         this.setInitialState(initialSoution);
@@ -32,7 +46,41 @@ public class LocalOptimal implements LocalSearch {
         this.elementHasBeenReplaced = Boolean.FALSE;
     }
 
-    private void addElement() {}
+    private void addElement() {
+        final var elementsOfCurrentSolution = this.currentSolution.getElements();
+        final var allSuccessors = new HashSet<>(elementsOfCurrentSolution
+            .stream()
+            .flatMap(node -> this.problem.getGraph().successors(node).stream())
+            .toList()
+        );
+        final var allPossibleCandidates = new HashSet<>(allSuccessors
+            .stream()
+            .filter(node -> !elementsOfCurrentSolution.contains(node))
+            .toList()
+        );
+        final var remainingCapacity = this.problem.getCapacity().subtract(this.currentSolution.getWeight());
+        final var elementWeightMap = this.problem.getWeightMap();
+        final var candidates = allPossibleCandidates
+            .stream()
+            .filter(node -> elementsOfCurrentSolution.containsAll(
+                this.problem.getGraph().predecessors(node)
+            ))
+            .filter(node -> !elementWeightMap.get(node).exceeds(remainingCapacity));
+        final var bestCandidate = candidates
+            .map(node -> new ElementValuePair<Double>(node, greedyCriteria.evaluate(this.problem, this.currentSolution, node)))
+            .min((lhs, rhs) -> (int) Math.signum(lhs.value - rhs.value))
+            .map(pair -> pair.element);
+        if (bestCandidate.isPresent()) {
+            this.elementHasBeenAdded = Boolean.TRUE;
+            final var elementToAdd = bestCandidate.get();
+            final var elementWeight = this.problem.getWeightMap().get(elementToAdd);
+            this.currentSolution.addElement(elementToAdd, elementWeight);
+        }
+        else {
+            this.elementHasBeenAdded = Boolean.FALSE;
+        }
+        return;
+    }
 
     private void replaceElement() {}
 
