07d77ba71fd0493870f42635348cdcbb9a6b95bf
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-07-14 14:12:14 -0300

    feat(project): implement method 'addElement' of 'LocalOptimal'

--- project/GRASP/src/test/java/localsearch/TestLocalOptimal.java
+++ project/GRASP/src/test/java/localsearch/TestLocalOptimal.java
@@ -7,6 +7,8 @@ import org.json.JSONException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import metaheuristic.greedy_criteria.GreedyCriteria;
+import metaheuristic.greedy_criteria.MaximumCombinedWeight;
 import problem.InvalidInputException;
 import problem.Problem;
 import problem.ProblemData;
@@ -20,26 +22,28 @@ public class TestLocalOptimal {
     static final String CASES_DIR = RESOURCES_DIR + "localsearch/";
 
     Problem problem;
+    GreedyCriteria greedyCriteria;
 
     public void init(final String instanceName) throws JSONException, IOException, InvalidInputException {
         this.problem = new ProblemData(CASES_DIR + instanceName);
+        this.greedyCriteria = new MaximumCombinedWeight();
     }
 
     @Test
-    public void testConstructiveGraspInstance1() throws JSONException, IOException, InvalidInputException {
+    public void testCase1() throws JSONException, IOException, InvalidInputException {
         this.init("case_1.json");
         final Solution initialSolution = new WeightedSolution(this.problem.getCapacity());
         for (final var element : new Integer[]{0, 1}) {
             initialSolution.addElement(element, this.problem.getWeightMap().get(element));
         }
-        final LocalSearch localOptimalRunner = new LocalOptimal();
+        final LocalSearch localOptimalRunner = new LocalOptimal(this.problem, this.greedyCriteria);
         final Solution localOptimalSolution = localOptimalRunner.search(initialSolution);
         System.out.println(localOptimalSolution);
         Assert.assertTrue(problem.getGraph().nodes().containsAll(localOptimalSolution.getElements()));
         Assert.assertTrue(localOptimalSolution.getElements().size() > 0);
         Assert.assertEquals(localOptimalSolution.getCost(), (Integer) 3);
         Assert.assertFalse(localOptimalSolution.getWeight().exceeds(problem.getCapacity()));
-        Assert.assertEquals(localOptimalSolution.getWeight(), new Weight(Arrays.asList(200, 200)));
+        Assert.assertEquals(localOptimalSolution.getWeight(), new Weight(Arrays.asList(160, 160)));
     }
 
 }
