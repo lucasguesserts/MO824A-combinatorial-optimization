e97b5c7c8d3b5211174987895646f666a341d561
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-07-14 21:37:48 -0300

    chore(project): make 'generate_small_instance' to get command line args

--- project/tools/generate_small_instance.py
+++ project/tools/generate_small_instance.py
@@ -1,24 +1,79 @@
+import argparse
 import numpy as np
 from MUPKP import FileManipulation
 from MUPKP import RandomizedInstanceGenerator
 
+parser = argparse.ArgumentParser(
+    description='Generate a MUPKP problem instance'
+)
+parser.add_argument(
+    "--nn",
+    type=int,
+    default=10,
+    help="number_of_nodes",
+    required=False,
+)
+parser.add_argument(
+    "--ep",
+    type=float,
+    default=0.3,
+    help="edge_probability",
+    required=False,
+)
+parser.add_argument(
+    "--ws",
+    type=int,
+    default=3,
+    help="weight_size",
+    required=False,
+)
+parser.add_argument(
+    "--pn",
+    type=float,
+    default=0.5,
+    help="percentage_of_nodes_to_fit",
+    required=False,
+)
+parser.add_argument(
+    "--min",
+    type=int,
+    default=500,
+    help="weight_minimum_value",
+    required=False,
+)
+parser.add_argument(
+    "--max",
+    type=int,
+    default=1000,
+    help="weight_maximum_value",
+    required=False,
+)
+parser.add_argument(
+    "--file",
+    type=str,
+    default="foo",
+    help="name of the file to save, without extension",
+    required=False,
+)
 
-default_json_file_name = "foo.json"
-default_figure_file_name = "foo.png"
+args = parser.parse_args()
+print(f"\nargs: {args}\n")
 
 generator = RandomizedInstanceGenerator(
-    number_of_nodes=7,
-    edge_probability=0.3,
-    weight_size=3,
-    percentage_of_nodes_to_fit=0.5,
-    weight_minimum_value=500,
-    weight_maximum_value=1000,
+    number_of_nodes=args.nn,
+    edge_probability=args.ep,
+    weight_size=args.ws,
+    percentage_of_nodes_to_fit=args.pn,
+    weight_minimum_value=args.min,
+    weight_maximum_value=args.max,
 )
 
 instance = generator.generate()
-FileManipulation.dict_to_json(instance.to_dict(), default_json_file_name)
 
-instance.plot(default_figure_file_name)
+json_file_name = f"{args.file}.json"
+figure_file_name = f"{args.file}.png"
+FileManipulation.dict_to_json(instance.to_dict(), json_file_name)
+instance.plot(figure_file_name)
 
 print(f"weights:\n{instance.weights}")
 print()
@@ -27,5 +82,5 @@ print()
 print(f"sum of all weights: {np.sum(instance.weights, axis=1)}")
 print(f"weight capacity: {instance.capacity}")
 print()
-print(f"instance file: {default_json_file_name}")
-print(f"instance figure: {default_figure_file_name}")
+print(f"instance file: {json_file_name}")
+print(f"instance figure: {figure_file_name}")
