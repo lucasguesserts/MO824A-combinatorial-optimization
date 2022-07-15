e13e40292b179e5088289255d67a4fbf34eeeead
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-07-14 23:27:17 -0300

    feat(project): implement greedy algorithm

---
+++ project/java/greedy/src/main/java/Main.java
@@ -0,0 +1,58 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.json.JSONException;
+
+import localsearch.LocalOptimal;
+import metaheuristic.ConstructiveGrasp;
+import metaheuristic.greedy_criteria.GreedyCriteriaMax;
+import problem.InvalidInputException;
+import problem.ProblemData;
+import solution.Solution;
+
+public class Main {
+
+    static final String INSTANCES_DIR = "../../instances/small_examples/";
+
+    public static void main(final String[] args) throws JSONException, IOException, InvalidInputException {
+        final var listOfInstances = listFiles(INSTANCES_DIR);
+        for (final var instanceName : listOfInstances) {
+            System.out.println("\n\n========== START ==========\n");
+            System.out.println("Instance " + instanceName);
+            try{
+                final var solution = solve(instanceName);
+                System.out.println("Solution:");
+                System.out.println(solution);
+            } catch (final Exception exception) {
+                System.out.println("\nerror processing instance");
+                System.out.println(exception);
+            }
+            System.out.println("\n\n========== END ==========");
+        }
+    }
+
+    private static Solution solve(final String instanceName) throws JSONException, IOException, InvalidInputException {
+        final var problem = new ProblemData(instanceName);
+        final var greedyCriteria = new GreedyCriteriaMax();
+        final Double greedyParameter = 0.0;
+        final var grasp = new ConstructiveGrasp(problem, greedyCriteria, greedyParameter);
+        final var solution = grasp.constructiveHeuristic();
+        final var localOptimalRunner = new LocalOptimal(problem, greedyCriteria);
+        final Solution localOptimalSolution = localOptimalRunner.search(solution);
+        return localOptimalSolution;
+    }
+
+    private static List<String> listFiles(final String dir) {
+        final File directoryPath = new File(dir);
+        final File filesList[] = directoryPath.listFiles();
+        final List<String> listOfFiles = Arrays.asList(filesList)
+            .stream()
+            .map(file -> file.getAbsolutePath())
+            .filter(fileName -> fileName.contains(".json"))
+            .toList();
+        return listOfFiles;
+    }
+
+}
